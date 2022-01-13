1bbeaa486e1ab3b4e9ee8fdee09ed437c34866af
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-13 23:38:07 +0100

    update user page

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+
+const router = express.Router()
+
+const User = require('../models/user')
+// const Restaurant = require('../models/restaurant')
+// const Order = require('../models/order')
+
+const ayse = new User('ayse', 32)
+const tuna = new User('tuna', 2)
+
+const users = [ayse, tuna]
+console.log(users)
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', (req, res) => {
+  let results = users
 
-module.exports = router;
+  if (req.query.name) {
+    results = users.filter(user => user.name == req.query.name)
+  }
+
+  res.send(results)
+})
+
+router.get('/:userId', (req, res) => {
+  const user = users[req.params.userId]
+
+  if (user) res.render('user', { user })
+  else res.sendStatus(404)
+})
+
+module.exports = router
