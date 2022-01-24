86517ef460d874c2d659aae3dd314c491e576d9f
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-24 12:33:30 +0100

    refactor users page base on mongoonese

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,31 +3,59 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('../models/user')
-// const Restaurant = require('../models/restaurant')
-// const Order = require('../models/order')
-
-const ayse = new User('ayse', 32)
-const tuna = new User('tuna', 2)
-
-const users = [ayse, tuna]
-console.log(users)
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  let results = users
+router.get('/', async (req, res) => {
+  // const { name, age } = req.body
+  const query = {}
 
   if (req.query.name) {
-    results = users.filter(user => user.name == req.query.name)
+    query.name = req.query.name
   }
 
-  res.send(results)
+  if (req.query.age) {
+    query.age = req.query.age
+  }
+
+  res.send(await User.find(query))
 })
 
-router.get('/:userId', (req, res) => {
-  const user = users[req.params.userId]
+router.get('/initialize', async (req, res) => {
+  const ayse = await User.create({ name: 'ayse', age: 32 })
+  const tuna = await User.create({ name: 'tuna', age: 2 })
+
+  console.log(ayse, tuna)
+  res.sendStatus(200)
+})
+
+router.get('/:userId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
 
   if (user) res.render('user', { user })
   else res.sendStatus(404)
+})
+
+router.post('/', async (req, res) => {
+  const { name, age } = req.body
+
+  const user = await User.create({ name, age })
+  res.send(user)
+})
+
+router.delete('/:id', async (req, res) => {
+  await User.findByIdAndDelete(req.params.id)
+  res.sendStatus(200)
+})
+
+router.patch('/:id', async (req, res) => {
+  let user
+  if (req.body.name) {
+    user = await User.findByIdAndUpdate(req.params.id, { name: req.body.name })
+  } else if (req.body.age) {
+    user = await User.findByIdAndUpdate(req.params.id, { age: req.body.age })
+  }
+
+  res.send(user)
 })
 
 module.exports = router
