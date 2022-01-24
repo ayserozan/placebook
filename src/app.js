375cdfc2cbe36fffb29317fc54032292514bc1b2
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-24 12:25:38 +0100

    move app.js in the src folder

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+require('./database-connection')
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes')
 const usersRouter = require('./routes/users')
 const restaurantRouter = require('./routes/restaurants')
 
