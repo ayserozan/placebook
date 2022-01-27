09eed5ff36a0ece7ab4cddb91dbe6c9036274bb9
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-27 01:04:24 +0100

    convert the restaurant object from class to mongoose.Schema

--- src/models/restaurant.js
+++ src/models/restaurant.js
@@ -1,16 +1,32 @@
+// const res = require('express/lib/response')
+const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
 const Menu = require('./menu')
 
+const restaurantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  likedBy: [],
+  menus: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Menu',
+      autopopulate: true,
+    },
+  ],
+})
 class Restaurant {
-  constructor(name) {
-    this.name = name
-    this.likedBy = []
-    this.menus = []
-  }
+  async createMenu(menuName) {
+    const menu = await Menu.create({ name: menuName })
+    await this.menus.push(menu)
 
-  createMenu(menuName) {
-    // const menu = new Menu(menuName)
-    this.menus.push(new Menu(menuName))
+    await this.save()
   }
 }
 
-module.exports = Restaurant
+restaurantSchema.loadClass(Restaurant)
+restaurantSchema.plugin(autopopulate)
+module.exports = mongoose.model('Restaurant', restaurantSchema)
