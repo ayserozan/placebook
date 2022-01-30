2afe08864dd716eee083f1632992e16410907888
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-30 18:21:46 +0100

    create restaurant card component

---
+++ frontend/src/components/restaurant-card.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "RestaurantCard",
+  props: ["restaurant"],
+};
+</script>
+
+<template lang="pug">
+    .box
+        h2 {{restaurant.name}}
+
+        h3 {{restaurant.cuisines.map( x => x).join(', ')}}
+</template>
+
+<style lang="scss" scoped>
+.box {
+  margin-left: 25rem;
+  margin-right: 15rem;
+  border: 1px solid #333;
+  background-image: linear-gradient(
+    to bottom right,
+    rgb(255, 20, 147) 10%,
+    rgb(249, 105, 14)
+  );
+  border-radius: 0.3rem;
+  text-indent: 100px;
+
+  h3 {
+    font-family: "Lucida Console", "Courier New", monospace;
+    font-size: 12px;
+    color: #aaf;
+  }
+}
+</style>
