70e18fcc41895ae2109e428dafca47648b26663c
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-30 18:21:26 +0100

    add logo nav bar

---
+++ frontend/src/app.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  #app
+    div
+    #nav
+      img(src="@/assets/logo.png")
+      router-link(to="/") Home |&nbsp;
+      router-link(to="/about") About
+    <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav {
+  text-align: right;
+  background-image: linear-gradient(
+    to bottom right,
+    rgb(252, 6, 137) 20%,
+    rgb(249, 105, 14)
+  );
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    text-align: left;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+  img {
+    padding-right: 70rem;
+    text-align: left;
+  }
+}
+</style>
