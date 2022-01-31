c6a8aee9cf05e4ea446b446f26fa2f5c540c29d8
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-31 15:40:35 +0100

    move files into restaurant files

---
+++ frontend/src/views/restaurant-detail.vue
@@ -0,0 +1,33 @@
+<script>
+import RestaurantCard from "@/components/restaurant-card.vue";
+import Counter from "@/components/counter.vue";
+import axios from "axios";
+import { mapActions } from "vuex";
+
+export default {
+  name: "RestaurantDetail",
+  components: { RestaurantCard, Counter },
+  data() {
+    return {
+      restaurant: null,
+    };
+  },
+  async created() {
+    //const restaurantRequest = await axios.get(`/api/restaurants/`);
+    //this.restaurant = restaurantRequest.data;
+    console.log("This returns:", this.$route.params.id);
+    this.restaurant = await this.fetchRestaurant(this.$route.params.id);
+    console.log("Returned Restaurant:", this.restaurant);
+  },
+  methods: {
+    ...mapActions(["fetchRestaurant"]),
+  },
+};
+</script>
+
+<template lang="pug">
+  .about
+    h1 This is a restaurant detail
+    RestaurantCard(:restaurant="restaurant" v-if="restaurant")
+    Counter
+</template>
---
+++ frontend/src/views/restaurant-list.vue
@@ -0,0 +1,35 @@
+<script>
+//import axios from "axios";
+import Counter from "@/components/counter.vue";
+import { mapActions } from "vuex";
+
+export default {
+  name: "RestaurantList",
+  components: { Counter },
+  data() {
+    return {
+      restaurants: [],
+    };
+  },
+
+  async created() {
+    //const restaurantRequest = await axios.get("/api/restaurants");
+    //this.restaurants = restaurantRequest.data;
+    console.log(this.$route);
+    this.restaurants = await this.fetchRestaurant();
+  },
+  methods: {
+    ...mapActions(["fetchRestaurant"]),
+  },
+};
+</script>
+
+<template lang="pug">
+  .home
+    h1 Restaurant List
+    div(v-for="restaurant in restaurants")
+      router-link(:to="`/restaurants/${restaurant._id}`") {{restaurant.name}} and {{restaurant._id}}
+    Counter
+</template>
+
+<style lang="scss" scoped></style>
