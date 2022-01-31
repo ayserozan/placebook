355bde123ad9060e22346ce859fab8722859900f
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-01-31 15:43:35 +0100

    create fetchRestaurant methods

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
+const mutations = {
+  INCREMENT_COUNT: "increment count",
+};
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    count: 0,
+  },
+  mutations: {
+    [mutations.INCREMENT_COUNT](state) {
+      state.count++;
+    },
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit(mutations.INCREMENT_COUNT);
+    },
+    async fetchRestaurant(id) {
+      const restaurantRequest = await axios.get(`/api/restaurants/${id}`);
+      return restaurantRequest.data;
+    },
+    async fetchRestaurant() {
+      const restaurantsRequest = await axios.get("/api/restaurants");
+      return restaurantsRequest.data;
+    },
+  },
   modules: {},
 });
