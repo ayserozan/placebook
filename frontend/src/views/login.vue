0c4127adc25b8465de1ced3aebd5b961dc4efa4c
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-02-06 21:08:40 +0100

    change the login page visualization

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -34,11 +34,41 @@ export default {
 .login
     form(@submit="submitLogin")
       h1 Log in to your account
-      label(for="email") Email:&nbsp;
-        input(v-model="email" id="email" type="email" placeholder="Your email" required)
-      label(for="password") Password:&nbsp;
-        input(v-model="password" id="password" type="password" placeholder="Your password" required)
+      #email-content
+        div
+          label(for="email") Email:&nbsp;
+        div
+          input(v-model="email" id="email" type="email" placeholder="Your email" required)
+      #password-content
+        div
+          label(for="password") Password:&nbsp;
+        div
+          input(v-model="password" id="password" type="password" placeholder="Your password" required)
       input(type="submit" text="Log in")
     div(v-if="backendError") {{ backendError }}
     div Don't have an account yet? <router-link to="/register">Register</router-link>
 </template>
+
+<style lang="scss" scoped>
+.login {
+  display: block;
+  margin: 1rem 0;
+  text-align: center;
+  margin-left: 23rem;
+  margin-right: 20rem;
+  width: 20rem;
+  height: 300px;
+  div {
+    label {
+      float: left;
+      width: 100px;
+      text-align: left;
+      padding-top: 10px;
+    }
+    input {
+      float: right;
+      padding-top: 10px;
+    }
+  }
+}
+</style>
