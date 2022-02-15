e5e3ca65ff9186912e9b017536e7c3ecd50f2aa5
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-02-15 11:11:48 +0100

    add logo image and cuisine information

--- frontend/src/components/restaurant-card.vue
+++ frontend/src/components/restaurant-card.vue
@@ -1,31 +1,37 @@
 <script>
 export default {
-  name: "RestaurantCard",
-  props: ["restaurant"],
-};
+  name: 'RestaurantCard',
+  props: ['restaurant'],
+  methods: {
+    getLogo(logo) {
+      console.log(logo)
+      return require(`@/assets/'${logo}`)
+    },
+  },
+}
 </script>
 
 <template lang="pug">
     .box
-        h2 {{restaurant.name}}
-        h3 {{restaurant.cuisines.map(cuisine => cuisine ).join(', ')}}
+        img(:src="require(`@/assets/${restaurant.logo}`)", style='width:100px; height:100px;')
+        div
+          h2 {{restaurant.name.toUpperCase()}}
+          h3 {{restaurant.cuisines.map(cuisine => cuisine ).join(', ')}}
 </template>
 
 <style lang="scss" scoped>
 .box {
+  background-image: linear-gradient(to bottom right, rgb(255, 20, 147) 10%, rgb(249, 105, 14));
+  display: inline-flex;
+  width: 97%;
   margin-left: 1rem;
-  margin-right: 1rem;
   border: 1px solid #333;
-  background-image: linear-gradient(
-    to bottom right,
-    rgb(255, 20, 147) 10%,
-    rgb(249, 105, 14)
-  );
-  border-radius: 0.3rem;
-  text-indent: 100px;
-
+  h2 {
+    text-indent: 1rem;
+  }
   h3 {
-    font-family: "Lucida Console", "Courier New", monospace;
+    text-indent: 1rem;
+    font-family: 'Lucida Console', 'Courier New', monospace;
     font-size: 12px;
     color: #aaf;
   }
