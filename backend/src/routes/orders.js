9fc7b40b0ee3a179b5961546498f83d79c58f424
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-02-17 11:56:07 +0100

    update post request

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const Order = require('../models/order')
-const Product = require('../models/product')
+// const User = require('../models/user')
+// const Product = require('../models/product')
 
 const router = express.Router()
 
-async function main() {
+/* async function main() {
   const order1 = await Order.create({ item: [], quantity: [], amount: 0 })
   const margaritta = await Product.create({ name: 'margaritta', price: 10 })
   const cola = await Product.create({ name: 'Coca-cola', price: 3 })
@@ -14,20 +15,27 @@ async function main() {
   await order1.calculateAmount()
 
   return order1
-}
+} */
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
   // const { name, age } = req.body
-  const order = await main()
+  // const order = await main()
+  const order = await Order.find()
   res.send(order)
 })
 
 router.post('/', async (req, res) => {
-  const { item, quantity } = req.body
+  const { orderItems, userId } = req.body
 
-  const order = await Order.create({ item, amount: 0, quantity })
+  const createdOrder = await Order.create({ orderItems, userId, amount: 0 })
+  // we created 33 line because of the mongoose autopopulate
+  const order = await Order.findById(createdOrder._id)
+
+  await order.calculateAmount()
   res.send(order)
 })
+
+router.patch('/:id', async (req, res) => {})
 
 module.exports = router
