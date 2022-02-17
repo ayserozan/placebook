0605552b4db9ca7781b35979122076803db2b98d
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-02-17 11:58:05 +0100

    add + button and create order function

--- frontend/src/components/menu-card.vue
+++ frontend/src/components/menu-card.vue
@@ -1,35 +1,62 @@
 <script>
+import axios from 'axios'
+import { mapState } from 'vuex'
+
 export default {
-  name: "MenuCard",
-  props: ["menu"],
-};
+  name: 'MenuCard',
+  props: ['menu'],
+  mounted: {
+    createEmptyOrder(user) {
+      const userId = user._id
+      axios.post('/api/orders', { orderItems: { item: product._id, quantity: 1 }, userId })
+    },
+  },
+  methods: {
+    createNewOrder(product, user) {
+      const userId = user._id
+      axios.post('/api/orders', { orderItems: { item: product._id, quantity: 1 }, userId })
+    },
+  },
+  computed: {
+    ...mapState(['item', 'user']),
+  },
+}
 </script>
 
 <template lang="pug">
     .box
-        h2 {{menu.name}}
-
-        .product(v-for="product in menu.product")
-          h3 - {{product.name}}
-
+      .menuInfo
+        h2 {{menu.name.toUpperCase()}}
+      .product(v-for="product in menu.product")
+        h3 - {{product.name}}
+        h3 {{product.price}} €
+        div
+          <!--button(@click='deleteProduct') - -->
+          <!--button() {{item}} -->
+          button(@click='createNewOrder(product, user)') +
 </template>
 
 <style lang="scss" scoped>
 .box {
   margin-left: 1rem;
   margin-right: 1rem;
-  border: 1px solid #333;
-  background-image: linear-gradient(
-    to bottom right,
-    rgb(245, 139, 195) 10%,
-    rgb(238, 160, 111)
-  );
-  border-radius: 0.3rem;
 
-  h3 {
-    font-family: "Lucida Console", "Courier New", monospace;
-    font-size: 12px;
-    color: rgb(38, 38, 168);
+  .menuInfo {
+    margin-top: 1rem;
+    border: 1px solid #333;
+    background-color: rgb(245, 222, 218);
+  }
+  .product {
+    border: 1px solid #333;
+    background-color: rgb(253, 252, 249);
+    h3 {
+      font-family: 'Lucida Console', 'Courier New', monospace;
+      font-size: 12px;
+      color: rgb(38, 38, 168);
+    }
+    div {
+      text-align: end;
+    }
   }
 }
 </style>
