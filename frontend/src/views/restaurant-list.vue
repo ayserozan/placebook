e3c7c7c764eb10a6115d20338ffe33738f6e4a61
Ayse Erozan <ayses.aydogdu@gmail.com>
2022-02-21 13:03:00 +0100

    move RestaurantCard to restaurant list

--- frontend/src/views/restaurant-list.vue
+++ frontend/src/views/restaurant-list.vue
@@ -1,33 +1,46 @@
 <script>
 //import axios from "axios";
-import Counter from "@/components/counter.vue";
-import { mapActions } from "vuex";
+import RestaurantCard from '@/components/restaurant-card.vue'
+import Counter from '@/components/counter.vue'
+import { mapActions } from 'vuex'
 
 export default {
-  name: "RestaurantList",
-  components: { Counter },
+  name: 'RestaurantList',
+  components: { Counter, RestaurantCard },
   data() {
     return {
       restaurants: [],
-    };
+    }
   },
 
   async created() {
-    console.log(this.$route);
-    this.restaurants = await this.fetchRestaurants();
+    console.log(this.$route)
+    this.restaurants = await this.fetchRestaurants()
   },
   methods: {
-    ...mapActions(["fetchRestaurants"]),
+    ...mapActions(['fetchRestaurants']),
   },
-};
+}
 </script>
 
 <template lang="pug">
   .home
     h1 Restaurant List
     div(v-for="restaurant in restaurants")
-      router-link(:to="`/restaurants/${restaurant._id}`") {{restaurant.name}}
+      router-link(:to="`/restaurants/${restaurant._id}`")
+        RestaurantCard(:restaurant="restaurant" v-if="restaurant")
     Counter
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  a {
+    font-weight: bold;
+    text-decoration: none;
+    color: rgb(248, 236, 246);
+    &.router-link-exact-active {
+      color: #0f0b07;
+    }
+  }
+}
+</style>
